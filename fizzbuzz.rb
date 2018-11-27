f89f32201d7730cc00310cb71c6e1c37c4a6b500
Timothy Tan <timtan93@gmail.com>
2018-11-27 09:20:53 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
-  if int % 5 && int % 3 == 0 
+  if int % 5 == 0 && int % 3 == 0 
     "FizzBuzz"
   elsif int % 3 == 0 # if the number int is divisible by 3
     "Fizz" # Go fizz
