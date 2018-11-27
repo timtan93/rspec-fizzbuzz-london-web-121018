91eb5c8814236960554b57017daa67821c192b43
Timothy Tan <timtan93@gmail.com>
2018-11-27 09:17:18 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(x)
-  if x/3 == 0 
+  if #{x}/3 == 0 
   puts "Fizz"
 end
