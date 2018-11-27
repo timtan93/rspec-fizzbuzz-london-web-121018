0c7264a2ed61c8b0b274620b91d8525d134b9815
Timothy Tan <timtan93@gmail.com>
2018-11-27 09:20:15 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,7 +3,7 @@
 def fizzbuzz(int)
   if int % 5 && int % 3 == 0 
     "FizzBuzz"
-  elseif int % 3 == 0 # if the number int is divisible by 3
+  elsif int % 3 == 0 # if the number int is divisible by 3
     "Fizz" # Go fizz
   elsif int % 5 == 0
     "Buzz"
